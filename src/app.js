828faeefc37af0eca037abe386a6abfc8be218ec
Nikhil06 <nikhil.v.yadav@apisero.com>
2025-06-11 17:07:37 +0530

    exploring routing get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 
 
-app.use((req,res) =>{
-    res.send("Hii from the server")
-});
+app.get("/user",(req,res) =>{
+    res.send({firstName:'nikhil', lastName:"yadav"})
+})
+
+app.post("/user",(req,res) =>{
+    res.send("Data is succesfully saved to the database");
+})
+
+app.use("/hello", (req,res) =>{
+     res.send("hello nikhil");
+})
+
 app.listen(5000, () =>{
     console.log(" is successfully running on port");
 });
