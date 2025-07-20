43cf2ed0b7e8052332e793cd636ad4fe195f99cf
Nikhil06 <nikhil.v.yadav@apisero.com>
2025-07-20 23:40:36 +0530

    console add

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
 
-app.use("/user", (req,res, next) =>{
-    // res.send("handling 1 rourter");
-    console.log("Hnalding router 1");
-    next();
-},
-(req,res, next) =>{
+// app.use("/user", (req,res, next) =>{
+//     // res.send("handling 1 rourter");
+//     console.log("Hnalding router 1");
+//     next();
+// },
+// (req,res, next) =>{
     
-     console.log("2 handler print");
-     next();
-},
-(req,res) =>{
-    res.send("3 handler")
-}
-);
+//      console.log("2 handler print");
+//      next();
+// },
+// (req,res) =>{
+//     res.send("3 handler")
+// }
+// );
 
 
 
-app.listen(5000, () =>{
-    console.log(" is successfully running on port");
-});
+// app.listen(5000, () =>{
+//     console.log(" is successfully running on port");
+// });
 
+console.log("deepti bhai")
